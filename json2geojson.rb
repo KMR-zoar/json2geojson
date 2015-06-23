21b4e73c56fc23bf5934bcf2f37a41e5f0198ddd
Zoar <zoar@k-side.net>
2015-06-23 16:31:16 +0900

    EPSGコードの指定を追加
    座標系番号を指定するとEPSGコードが変化するように変更した

--- json2geojson.rb
+++ json2geojson.rb
@@ -2,7 +2,11 @@
 # coding: utf-8
 require "json"
 
+code2epsg = ["", "2443", "2444", "2445", "2446", "2447", "2448", "2449", "2450", "2451", "2452", "2453", "2454", "2455", "2456", "2457", "2458", "2459", "2460", "2461"]
+
 json_file_path = ARGV[0]
+
+epsg_code = code2epsg[ARGV[1].to_i]
 
 json_data = open(json_file_path) do |io|
    JSON.load(io)
@@ -13,7 +17,7 @@ geojson_data = {
    'crs' => {
       'type' => 'name',
       'properties' => {
-         'name' => 'urn:ogc:def:crs:EPSG::2451'
+         'name' => 'urn:ogc:def:crs:EPSG::' + epsg_code
       }
    },
    'features' => []
@@ -30,7 +34,7 @@ json_data['data'].each do |data|
    }
 end
 
-geojson_file_path = json_file_path.gsub('.json','.geo.json')
+geojson_file_path = json_file_path.gsub('.json','.geojson')
 
 open(geojson_file_path, 'w') do |io|
    JSON.dump(geojson_data, io)
